cdcb4fc81daf61891a5755de7cdc486bb6cc65b7
hi-shadow <vazagautam54@gmail.com>
2023-01-19 00:09:37 +0530

    Intial Commit

--- FirstJava.java
+++ FirstJava.java
@@ -3,9 +3,9 @@ public class FirstJava
     public static void main(String[] args)
     {
        //sagar is good boy 
-        int  s1 = "sagar is good boy";
-        string s2 =2;
-        int i1 = 2
-
+        String  s1 = "sagar is good boy";
+        // string s2 =2;
+        // int i1 = 2
+        System.out.println(s1);
     }
 }
